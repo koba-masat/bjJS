40beff3e5f7e9dfde4527c213d56e0f7e4323b2a
koba-masat <masatennis10140409@gmail.com>
2021-07-22 23:07:18 +0900

    ドローできるようになった

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ class Deck {
      * 2:dia
      * 3:club
      */
-    constructor() {
-        var markimg = ["♠", "♥", "♦", "♣"];
-        var numimg = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-        this.cards = new Array(4);
-        for (let i = 0; i < this.cards.length; i++) {
-            this.cards[i] = new Array(13).fill(1);
+    static markimg = ["♠", "♥", "♦", "♣"];
+    static numimg = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    static cards = new Array(4);
+
+    initDeck(){
+        for(let i = 0; i < 13; i++){
+            Deck.cards[i] = new Array(13).fill(1);
         }
     }
 
-    draw() {
+    static deal() {
         let index_array = new Array();
         for (let mark = 0; mark < 4; mark++) {
             for (let num = 0; num < 13; num++) {
@@ -32,21 +33,47 @@ class Deck {
     }
 }
 
-let deck = new Deck();
+class Human {
+    constructor(id){
+        this.id = id;
+    }
 
-let deal = (id) => {
-    let card = deck.draw();
-    $(id).append("mark："+card[0]+"<br>num："+card[1]);
+    getResult(){
+        return 1;
+    }
+
+    draw(){
+        let num = $('#'+this.id).find('div').length + 1;
+        let card = Deck.deal();
+        let divId = this.id+'-'+num;
+        $("#"+this.id).append('<div id="'+divId+'" class="face-up"></div>');
+        $("#"+divId).append("<p>mark："+card[0]+"<br>num："+card[1]+"</p>")
+    }
+}
+let deck = new Deck();
+deck.initDeck();
+let player = new Human("player");
+let dealer = new Human("dealer");
+let initGame = () => {
+    let init = (id) => {
+        let card = Deck.deal();
+        $(id).append("<p>mark："+card[0]+"<br>num："+card[1]+"</p>");
+    };
+    
+    init("#dealer-1");
+    init("#dealer-2");
+    init("#player-1");
+    init("#player-2");
+};
+window.onload = () => {
+    initGame();
+    //setResult("#dealer-result", getHumanResult());
 }
 
-window.onload = () => {
-    deal("#d-1");
-    deal("#d-2");
-    deal("#p-1");
-    deal("#p-2");
+let setResult = (human, result) => {
+    $(human).text(result);
 }
 
 $('#hit').on('click', () => {
-    $("#player").append('<div id="p-3" class="face-up"></div>');
-    deal("#p-3");
+    player.draw();
 });
